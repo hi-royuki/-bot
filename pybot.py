1193db2438a13e600d2eb851d1e495109957f10e
hi-royuki <hiro.0621.ito3835@gmail.com>
2022-03-09 19:37:17 +0900

    lesson34

--- pybot.py
+++ pybot.py
@@ -1,14 +1,23 @@
+# 挨拶の辞書データを定義する。
+bot_dict = {
+    'こんにちは': 'コンニチハ',
+    'ありがとう': 'アリガトウ',
+    'さようなら': 'サヨウナラ',
+}
+
 while True: # 常に条件を満たす
 # input関数でユーザーからの入力を受付、入力された値をcommand変数に保存
     command = input('pybot> ')
     # print(command)
-# 条件分岐であいさつを切り替える
-    if 'こんにちは' in command:
-        print('コンニチハ')
-    elif 'ありがとう' in command:
-        print('ドウイタシマシテ')
-    elif 'さようなら' in command:
-        print('サヨウナラ')
-        break #繰り返し処理を終了
-    else:
-        print('ナニヲイッテイルカワカラナイ')
+    response = '' #空文字列で初期化する
+    for message in bot_dict:
+        if message in command:
+            response = bot_dict[message]
+            break
+
+        if not response:
+            response = '何ヲ言ッテイルカ、ワカラナイ'
+        print(response)
+
+        if 'さようなら' in command:
+            break

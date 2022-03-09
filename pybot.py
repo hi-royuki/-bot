d8f7a3abb99d48fb1129861e46581344854745df
hi-royuki <hiro.0621.ito3835@gmail.com>
2022-03-09 19:11:20 +0900

    lesson33(in演算子とbreak文)

--- pybot.py
+++ pybot.py
@@ -1,4 +1,14 @@
 while True: # 常に条件を満たす
 # input関数でユーザーからの入力を受付、入力された値をcommand変数に保存
     command = input('pybot> ')
-    print(command)
+    # print(command)
+# 条件分岐であいさつを切り替える
+    if 'こんにちは' in command:
+        print('コンニチハ')
+    elif 'ありがとう' in command:
+        print('ドウイタシマシテ')
+    elif 'さようなら' in command:
+        print('サヨウナラ')
+        break #繰り返し処理を終了
+    else:
+        print('ナニヲイッテイルカワカラナイ')
